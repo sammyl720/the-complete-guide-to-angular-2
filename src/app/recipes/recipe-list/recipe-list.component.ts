cac43cc10b313af5f19d77dfd9822a25d97c7ffe
sam leider <sam.leider@hotmail.com>
2020-02-05 14:52:55 -0500

    completed section 3 of course: bootstrap layout

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model'
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+    new Recipe('A Second Test Recipe', 'This is simply a delicous recipe', 'https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032__340.jpg')
+  ]
   constructor() { }
 
   ngOnInit() {
