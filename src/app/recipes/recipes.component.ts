3773c5b0b0f7e7a7601c9dc2d3e2f1674e20339e
sam leider <sam.leider@hotmail.com>
2020-02-11 21:05:56 -0500

    updated project to use subjects instead of eventemitters section 14

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,21 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { RecipeService } from './recipe.service'
-import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.css'],
-  providers: [RecipeService]
+  styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
-  selectedRecipe: Recipe
-  constructor(private recipeService: RecipeService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.recipeService.recipeSelected.subscribe((recipe: Recipe) => {
-      this.selectedRecipe = recipe
-    })
   }
 
 }
