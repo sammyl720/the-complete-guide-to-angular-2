8252df335084b3eec83f3dee5108a440ede3106c
sam leider <sam.leider@hotmail.com>
2020-02-23 15:44:25 -0500

    complete authentication section 20

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipesResolverService } from './recipes/recipes-resolver.service';
 import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth/auth.guard';
 
 
 const routes: Route[] = [
@@ -18,6 +19,7 @@ const routes: Route[] = [
   {
     path: "recipes",
     component: RecipesComponent,
+    canActivate: [AuthGuard],
     children: [
       {
         path: '',
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -17,7 +17,7 @@ export interface AuthResponseData {
 })
 export class AuthService {
   user = new BehaviorSubject<User>(null)
-  
+  private tokenExpirationTimer:any
   constructor(private http: HttpClient, private router: Router){}
 
   signup(email: string, password:string){
@@ -42,15 +42,52 @@ export class AuthService {
     }))
   }
 
+  autoLogin(){
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'))
+
+    if(!userData) {
+      return
+    }
+    const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
+
+    if(loadedUser.token){
+      this.user.next(loadedUser)
+      // console.log(userData._tokenExpirationDate)
+      const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime()
+      // console.log(expirationDuration)
+      this.autoLogout(expirationDuration)
+    }
+  }
+
   logout(){
     this.user.next(null)
     this.router.navigate(['/auth'])
+    localStorage.removeItem('userData')
+    if(this.tokenExpirationTimer) {
+      clearTimeout(this.tokenExpirationTimer)
+    }
+    this.tokenExpirationTimer = null;
+  } 
+
+  autoLogout(expirationDuration: number) {
+    
+    this.tokenExpirationTimer = setTimeout(() => {
+      this.logout()
+    }, expirationDuration)
   }
+
   private handleAuthentication(email:string, userId: string, token:string, expiresIn: number){
     const expirationDate = new Date(new Date().getTime() + expiresIn * 1000)
       const user = new User(email, userId, token, expirationDate)
 
       this.user.next(user)
+      this.autoLogout(expiresIn * 1000)
+      localStorage.setItem('userData', JSON.stringify(user))
   }
   private handleError(errorRes: HttpErrorResponse){
     let errorMessage = 'An unknown error occured'
